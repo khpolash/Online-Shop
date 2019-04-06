126ce0e4446f0cd08c72c9e2b8166b581cf8a219
khpolash <polash250@gmail.com>
2019-04-06 12:24:40 +0600

    create_categories_table

--- _ide_helper.php
+++ _ide_helper.php
@@ -1,4 +1,5 @@
-<?php
+<?php /** @noinspection ALL */
+/** @noinspection PhpHierarchyChecksInspection */
 // @formatter:off
 
 /**
--- database/migrations/2019_04_06_013244_create_password_resets_table.php
+++ database/migrations/2019_04_06_013244_create_password_resets_table.php
@@ -11,7 +11,7 @@ class CreatePasswordResetsTable extends Migration
      *
      * @return void
      */
-    public function up():void
+    public function up()
     {
         Schema::create('password_resets', function (Blueprint $table) {
             $table->string('email')->index();
@@ -24,7 +24,7 @@ class CreatePasswordResetsTable extends Migration
      *
      * @return void
      */
-    public function down():void 
+    public function down()
     {
         Schema::dropIfExists('password_resets');
     }
